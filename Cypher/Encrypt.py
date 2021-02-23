40b835ed74809a39b3bfe33d14694464e67e3f1e
christian-hall <christian.hall395@gmail.com>
2021-02-22 19:41:53 -0500

    added line with key phrase for encryption

--- Cypher/Encrypt.py
+++ Cypher/Encrypt.py
@@ -2,11 +2,10 @@ import os
 import random
 
 def getRandomCharacters():
-    # main cypher options
     characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
               'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
               '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+',
-              '[', '{', ']', '}', '\\', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?']
+              '[', '{', ']', '}', '\\', ';', ':', "'", '"', ',', '<', '.', '>', '/', '?', ' ']
     random_characters = []
     while len(characters) > 0:
         random_selection = random.choice(characters)
@@ -14,19 +13,59 @@ def getRandomCharacters():
         characters.remove(random_selection)
     return random_characters
 
+def encrypt(key, file):
+    print("encrypting file...")
+    
+    
 def getKey():
-    print("getting base characters...")
     character_base = getRandomCharacters()
-    print("getting code characters...")
     character_code = getRandomCharacters()
-    print("creating key...")
-    key = []
+    
+    char_key = []
+    
     while len(character_base) > 0 and len( character_code) > 0:
         random_base = random.choice(character_base)
         random_code = random.choice(character_code)
-        key.append([random_base, random_code])
+        char_key.append([random_base, random_code])
         character_code.remove(random_code)
         character_base.remove(random_base)
+    base_idx, code_idx = 1,1
+    
+    while base_idx == code_idx:
+        base_idx, code_idx = random.randint(0, 101), random.randint(0, 101)
+    
+    key = []
+    line_idx = 0
+    while (line_idx < 94):
+        char_idx = 0
+        printline = ""
+        while char_idx < 100:
+            if char_idx == base_idx:
+                char_key_entry = char_key[line_idx]
+                printline = printline + char_key_entry[0]
+            elif char_idx == code_idx:
+                char_key_entry = char_key[line_idx]
+                printline = printline + char_key_entry[1]
+            else:
+                printline = printline + random.choice(getRandomCharacters())
+            char_idx = char_idx + 1
+        key.append(printline)
+        line_idx = line_idx + 1
+    # add line 95, with the phrase v3h-S6XH2v&}W5b<w1n~ embedded. int 0 - 80
+    key_idx, char_idx, printline = random.randint(-1, 80), 0, ""
+    while char_idx < 81:
+        if char_idx == key_idx:
+            printline = printline + "v3h-S6XH2v&}W5b<w1n~"
+        else:
+            printline = printline + random.choice(getRandomCharacters())
+        char_idx = char_idx + 1
+    
+    key.append(printline)
     return key
-
+    # concat a document of 95 lines, the last line being informaiton and the first 94 being gibberish with the key
+    
 key = getKey()
+for k in key:
+    print(k)
+print()
+print("Goodbye")
