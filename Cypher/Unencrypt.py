a3c3b32045d98fd99a5bc7c03d3e66eaa5dae15b
christian-hall <christian.hall395@gmail.com>
2021-02-28 16:12:32 -0500

    added file recognition

--- Cypher/Unencrypt.py
+++ Cypher/Unencrypt.py
@@ -1,7 +1,18 @@
 import os
 
-def unencrypt(filepath, keypath):
+def unencrypt(input_file, keypath):
     success = False
+    input_lines, key_lines = [], []
+    if os.path.isfile(input_file) and os.path.isfile(keypath):
+        # get key
+        with open(keypath) as file:
+            for line in file:
+                key_lines.append(line)
+        # get input file
+        with open(input_file) as file:
+            for line in file:
+                input_lines.append(line)
+        success = True
     # get key from keypath
     # unencrypt key to get unencryption
     # delete key automatically
@@ -14,11 +25,13 @@ print("==================================================")
 print()
 directory = "/Users/christian/Documents/cyph3r/"
 
-filepath = directory + input("What is the name of the cyr file you are encrypting: ") + ".cyr"
-keypath = directory + input("What is k3y number (k3y_???.cyr): ") + ".cyr"
+input_file = directory + input("What is the name of the cyr file you are unencrypting: ") + ".cyr"
+keypath = directory + "k3y_"+ input("What is k3y number (3 digits): ") + ".cyr"
+print(input_file)
+print(keypath)
 if ".cyr.cyr" in input_file:
     input_file = input_file[:len(input_file) - 4]
-success = unencrypt(filepath, keypath)
+success = unencrypt(input_file, keypath)
 if success == True:
     print("Encryption successful")
 else:
